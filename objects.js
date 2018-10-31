dfe6960ca20d184e7252a860903fb82531a366e4
David Sollesse <davidsollesse@gmail.com>
2018-10-31 10:33:38 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {
+
+  Beethoven: 'I am Beethoven',
+  "Lil Wayne": 'Symphony No. 5'
+}
+//Showing can string keys
+
+function updatePlaylist(playlist, artist, song) {
+  var playlist = {artist: song};
+  return
+}
+//used artistName and songTitle without checking prior values
